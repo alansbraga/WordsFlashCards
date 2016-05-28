658a30e9824b233f68d0c75336363b2033b5441d
Alan Braga <alansbraga@outlook.com>
2016-05-28 11:47:02 -0300

    Initial Commit

---
+++ src/WordsFlashCards.Domain/Word.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WordsFlashCards.Domain
+{
+    // This project can output the Class library as a NuGet Package.
+    // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
+    public class Word
+    {
+        private List<Tag> _tags;
+        public string Text { get; set; }
+        public string Description { get; set; }
+        public IEnumerable<Tag> Tags
+        {
+            get
+            {
+                return _tags;
+            }
+            protected set
+            {
+                _tags = new List<Tag>(value);
+            }
+        }
+
+        public Word()
+        {
+            _tags = new List<Tag>();
+        }
+    }
+}
---
+++ src/WordsFlashCards.Domain/interfaces/IInterpreter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WordsFlashCards.Domain.interfaces
+{
+    public interface IInterpreter
+    {
+        string Name();
+        IEnumerable<Word> Interprete();
+    }
+}
