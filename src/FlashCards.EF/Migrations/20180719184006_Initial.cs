2941ce0f5ad41704162d688c0b2b86785c470ccd
Alan Braga <alan.braga@poliview.com.br>
2018-07-19 16:11:37 -0300

    EF Migrations

---
+++ src/FlashCards.EF/Migrations/20180719184006_Initial.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FlashCards.EF.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Collection",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Collection", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FlashCard",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Question = table.Column<string>(nullable: true),
+                    Answer = table.Column<string>(nullable: true),
+                    Status = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FlashCard", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FlashCardCollection",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    FlashCardId = table.Column<int>(nullable: true),
+                    Occurrences = table.Column<int>(nullable: false),
+                    CollectionId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FlashCardCollection", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_FlashCardCollection_Collection_CollectionId",
+                        column: x => x.CollectionId,
+                        principalTable: "Collection",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_FlashCardCollection_FlashCard_FlashCardId",
+                        column: x => x.FlashCardId,
+                        principalTable: "FlashCard",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sample",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Text = table.Column<string>(nullable: true),
+                    FlashCardId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sample", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Sample_FlashCard_FlashCardId",
+                        column: x => x.FlashCardId,
+                        principalTable: "FlashCard",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FlashCardCollection_CollectionId",
+                table: "FlashCardCollection",
+                column: "CollectionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FlashCardCollection_FlashCardId",
+                table: "FlashCardCollection",
+                column: "FlashCardId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sample_FlashCardId",
+                table: "Sample",
+                column: "FlashCardId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FlashCardCollection");
+
+            migrationBuilder.DropTable(
+                name: "Sample");
+
+            migrationBuilder.DropTable(
+                name: "Collection");
+
+            migrationBuilder.DropTable(
+                name: "FlashCard");
+        }
+    }
+}
